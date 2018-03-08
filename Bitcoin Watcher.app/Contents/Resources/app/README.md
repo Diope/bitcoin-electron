03a6fdc31f925a11167c6680a0d3c5263f644d1e
Dion Pettaway <dionp@fullsail.edu>
2018-03-08 18:42:44 -0500

    OMG an Electron App much wow

---
+++ Bitcoin Watcher.app/Contents/Resources/app/README.md
@@ -0,0 +1,5 @@
+# Bitcoin Watcher
+
+Oh wow, my first Electron App. Nothing super amazing or serious but hey I made one and ze source code is all yours to make better!
+
+All it does it pull data for the current price of Bitcoin, I don't even have bitcoin or anything but hey it was fun!
--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({titleBarStyle: 'hiddenInset', width: 800, height: 600})
+  win = new BrowserWindow({titleBarStyle: 'hiddenInset', width: 800, height: 600, devTools: false})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -54,7 +54,7 @@ function createWindow () {
   Menu.setApplicationMenu(menu)
 
   // Open the DevTools.
-     win.webContents.openDevTools()
+     
 
   // Emitted when the window is closed.
   win.on('closed', () => {
